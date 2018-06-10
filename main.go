723bbbe3f237f7e048fb435f1f968fea82c0e779
Frederik Fix <ich@derfred.com>
2018-06-10 16:54:51 +0200

    add GC stats

--- main.go
+++ main.go
@@ -36,6 +36,112 @@ var (
 		Help: "Number of threads currently running",
 	})
 
+	count = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_count",
+		Help: "Number of all (minor+major) GC runs",
+	})
+	minorGcCount = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_minor_gc_count",
+		Help: "Number of minor GC runs",
+	})
+	majorGcCount = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_major_gc_count",
+		Help: "Number of major GC runs",
+	})
+
+	heapAllocatedPages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_allocated_pages",
+		Help: "puma_gc_heap_allocated_pages",
+	})
+	heapSortedLength = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_sorted_length",
+		Help: "puma_gc_heap_sorted_length",
+	})
+	heapAllocatablePages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_allocatable_pages",
+		Help: "puma_gc_heap_allocatable_pages",
+	})
+	heapAvailableSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_available_slots",
+		Help: "puma_gc_heap_available_slots",
+	})
+	heapLiveSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_live_slots",
+		Help: "puma_gc_heap_live_slots",
+	})
+	heapFreeSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_free_slots",
+		Help: "puma_gc_heap_free_slots",
+	})
+	heapFinalSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_final_slots",
+		Help: "puma_gc_heap_final_slots",
+	})
+	heapMarkedSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_marked_slots",
+		Help: "puma_gc_heap_marked_slots",
+	})
+	heapSweptSlots = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_swept_slots",
+		Help: "puma_gc_heap_swept_slots",
+	})
+	heapEdenPages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_eden_pages",
+		Help: "puma_gc_heap_eden_pages",
+	})
+	heapTombPages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_heap_tomb_pages",
+		Help: "puma_gc_heap_tomb_pages",
+	})
+	totalAllocatedPages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_total_allocated_pages",
+		Help: "puma_gc_total_allocated_pages",
+	})
+	totalFreedPages = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_total_freed_pages",
+		Help: "puma_gc_total_freed_pages",
+	})
+	totalAllocatedObjects = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_total_allocated_objects",
+		Help: "puma_gc_total_allocated_objects",
+	})
+	totalFreedObjects = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_total_freed_objects",
+		Help: "puma_gc_total_freed_objects",
+	})
+	mallocIncreaseBytes = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_malloc_increase_bytes",
+		Help: "puma_gc_malloc_increase_bytes",
+	})
+	mallocIncreaseBytesLimit = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_malloc_increase_bytes_limit",
+		Help: "puma_gc_malloc_increase_bytes_limit",
+	})
+	rememberedWbUnprotectedObjects = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_remembered_wb_unprotected_objects",
+		Help: "puma_gc_remembered_wb_unprotected_objects",
+	})
+	rememberedWbUnprotectedObjectsLimit = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_remembered_wb_unprotected_objects_limit",
+		Help: "puma_gc_remembered_wb_unprotected_objects_limit",
+	})
+	oldObjects = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_old_objects",
+		Help: "puma_gc_old_objects",
+	})
+	oldObjectsLimit = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_old_objects_limit",
+		Help: "puma_gc_old_objects_limit",
+	})
+	oldmallocIncreaseBytes = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_oldmalloc_increase_bytes",
+		Help: "puma_gc_oldmalloc_increase_bytes",
+	})
+	oldmallocIncreaseBytesLimit = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "puma_gc_oldmalloc_increase_bytes_limit",
+		Help: "puma_gc_oldmalloc_increase_bytes_limit",
+	})
+
 	//We use our own registry instead of the default to avoid the standard metrics
 	registry = prometheus.NewRegistry()
 )
@@ -45,10 +151,65 @@ type pumaStats struct {
 	RequestBacklog int `json:"backlog"`
 }
 
+type pumaGcStats struct {
+	Count                               int `json:"count"`
+	MinorGcCount                        int `json:"minor_gc_count"`
+	MajorGcCount                        int `json:"major_gc_count"`
+	HeapAllocatedPage                   int `json:"heap_allocated_pages"`
+	HeapSortedLength                    int `json:"heap_sorted_length"`
+	HeapAllocatablePage                 int `json:"heap_allocatable_pages"`
+	HeapAvailableSlot                   int `json:"heap_available_slots"`
+	HeapLiveSlot                        int `json:"heap_live_slots"`
+	HeapFreeSlot                        int `json:"heap_free_slots"`
+	HeapFinalSlot                       int `json:"heap_final_slots"`
+	HeapMarkedSlot                      int `json:"heap_marked_slots"`
+	HeapSweptSlot                       int `json:"heap_swept_slots"`
+	HeapEdenPage                        int `json:"heap_eden_pages"`
+	HeapTombPage                        int `json:"heap_tomb_pages"`
+	TotalAllocatedPage                  int `json:"total_allocated_pages"`
+	TotalFreedPage                      int `json:"total_freed_pages"`
+	TotalAllocatedObject                int `json:"total_allocated_objects"`
+	TotalFreedObject                    int `json:"total_freed_objects"`
+	MallocIncreaseByte                  int `json:"malloc_increase_bytes"`
+	MallocIncreaseBytesLimit            int `json:"malloc_increase_bytes_limit"`
+	RememberedWbUnprotectedObject       int `json:"remembered_wb_unprotected_objects"`
+	RememberedWbUnprotectedObjectsLimit int `json:"remembered_wb_unprotected_objects_limit"`
+	OldObject                           int `json:"old_objects"`
+	OldObjectsLimit                     int `json:"old_objects_limit"`
+	OldmallocIncreaseByte               int `json:"oldmalloc_increase_bytes"`
+	OldmallocIncreaseBytesLimit         int `json:"oldmalloc_increase_bytes_limit"`
+}
+
 func init() {
 	// Metrics have to be registered to be exposed:
 	registry.MustRegister(requestBacklog)
 	registry.MustRegister(runningThreads)
+	registry.MustRegister(count)
+	registry.MustRegister(minorGcCount)
+	registry.MustRegister(majorGcCount)
+	registry.MustRegister(heapAllocatedPages)
+	registry.MustRegister(heapSortedLength)
+	registry.MustRegister(heapAllocatablePages)
+	registry.MustRegister(heapAvailableSlots)
+	registry.MustRegister(heapLiveSlots)
+	registry.MustRegister(heapFreeSlots)
+	registry.MustRegister(heapFinalSlots)
+	registry.MustRegister(heapMarkedSlots)
+	registry.MustRegister(heapSweptSlots)
+	registry.MustRegister(heapEdenPages)
+	registry.MustRegister(heapTombPages)
+	registry.MustRegister(totalAllocatedPages)
+	registry.MustRegister(totalFreedPages)
+	registry.MustRegister(totalAllocatedObjects)
+	registry.MustRegister(totalFreedObjects)
+	registry.MustRegister(mallocIncreaseBytes)
+	registry.MustRegister(mallocIncreaseBytesLimit)
+	registry.MustRegister(rememberedWbUnprotectedObjects)
+	registry.MustRegister(rememberedWbUnprotectedObjectsLimit)
+	registry.MustRegister(oldObjects)
+	registry.MustRegister(oldObjectsLimit)
+	registry.MustRegister(oldmallocIncreaseBytes)
+	registry.MustRegister(oldmallocIncreaseBytesLimit)
 }
 
 func main() {
@@ -88,10 +249,10 @@ func main() {
 }
 
 func runServer(c *cli.Context) {
-
 	go func() {
 		for {
 			updateMetrics(fmt.Sprintf("%s/stats?token=%s", c.GlobalString("control-url"), c.GlobalString("auth-token")))
+			updateGcMetrics(fmt.Sprintf("%s/gc-stats?token=%s", c.GlobalString("control-url"), c.GlobalString("auth-token")))
 			time.Sleep(5 * time.Second)
 		}
 	}()
@@ -104,8 +265,52 @@ func runServer(c *cli.Context) {
 	fatalfOnError(err, "Failed to bind on %s: ", c.GlobalString("bind-address"))
 }
 
-func updateMetrics(url string) {
+func updateGcMetrics(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Failed to fetch metrics: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		log.Printf("Got error %s from control url", resp.Status)
+		return
+	}
 
+	var gcstats pumaGcStats
+	if err := json.NewDecoder(resp.Body).Decode(&gcstats); err != nil {
+		log.Printf("Error decoding response from control server: %s", err)
+	}
+
+	count.Set(float64(gcstats.Count))
+	minorGcCount.Set(float64(gcstats.MinorGcCount))
+	majorGcCount.Set(float64(gcstats.MajorGcCount))
+	heapAllocatedPages.Set(float64(gcstats.HeapAllocatedPage))
+	heapSortedLength.Set(float64(gcstats.HeapSortedLength))
+	heapAllocatablePages.Set(float64(gcstats.HeapAllocatablePage))
+	heapAvailableSlots.Set(float64(gcstats.HeapAvailableSlot))
+	heapLiveSlots.Set(float64(gcstats.HeapLiveSlot))
+	heapFreeSlots.Set(float64(gcstats.HeapFreeSlot))
+	heapFinalSlots.Set(float64(gcstats.HeapFinalSlot))
+	heapMarkedSlots.Set(float64(gcstats.HeapMarkedSlot))
+	heapSweptSlots.Set(float64(gcstats.HeapSweptSlot))
+	heapEdenPages.Set(float64(gcstats.HeapEdenPage))
+	heapTombPages.Set(float64(gcstats.HeapTombPage))
+	totalAllocatedPages.Set(float64(gcstats.TotalAllocatedPage))
+	totalFreedPages.Set(float64(gcstats.TotalFreedPage))
+	totalAllocatedObjects.Set(float64(gcstats.TotalAllocatedObject))
+	totalFreedObjects.Set(float64(gcstats.TotalFreedObject))
+	mallocIncreaseBytes.Set(float64(gcstats.MallocIncreaseByte))
+	mallocIncreaseBytesLimit.Set(float64(gcstats.MallocIncreaseBytesLimit))
+	rememberedWbUnprotectedObjects.Set(float64(gcstats.RememberedWbUnprotectedObject))
+	rememberedWbUnprotectedObjectsLimit.Set(float64(gcstats.RememberedWbUnprotectedObjectsLimit))
+	oldObjects.Set(float64(gcstats.OldObject))
+	oldObjectsLimit.Set(float64(gcstats.OldObjectsLimit))
+	oldmallocIncreaseBytes.Set(float64(gcstats.OldmallocIncreaseByte))
+	oldmallocIncreaseBytesLimit.Set(float64(gcstats.OldmallocIncreaseBytesLimit))
+}
+
+func updateMetrics(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("Failed to fetch metrics: %s", err)
